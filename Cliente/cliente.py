f27f181d892c08fe0fb10909a6ed9821ba57002c
ldefeo <ldefeo@fi.uba.ar>
2022-09-17 14:17:37 -0300

    Carga de archivos al servidor

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -7,3 +7,15 @@ clientSocket.sendto(message.encode(),(serverName,serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage.decode())
 clientSocket.close()
+
+def cargarArchivos(ruta):
+    listaLineas = []
+    with open(ruta,"r") as file:
+        for linea in file:
+            linea = linea.rstrip("\n").split(",")
+            listaLineas.append(linea)
+    for linea in listaLineas:
+        clientSocket.sendto(linea.encode(),(serverName,serverPort))
+        modifiedMessage, serverAddress, ack = clientSocket.recvfrom(2048)
+        
+        
