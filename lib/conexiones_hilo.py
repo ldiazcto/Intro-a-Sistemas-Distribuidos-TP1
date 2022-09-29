b9a94c900b932faa342e6da84467c162deee1bde
ldefeo <ldefeo@fi.uba.ar>
2022-09-28 22:28:41 -0300

    Me falto uno jaja

--- lib/conexiones_hilo.py
+++ lib/conexiones_hilo.py
@@ -70,13 +70,14 @@ class Conexion(threading.Thread):
         if (paquete.esDownload()):
             cargaPaquete = paquete.obtenerMensaje
             #Aca llamo a las funciones del cliente para manejo de paquetes y le paso el paquete OJO QUE HAY QUE GUARDAR LA RUTA EN LA CONEXION
-"""
-        if (self.gestor_paquete.cierreConexion(5)): #no me gusta se podria hacer a traves del gesto
+""" 
+        """
+        if (paquete.esACK()): #no me gusta se podria hacer a traves del gesto
             paquete_ack = self.gestor_paquete.crearPaqueteACK(1)
             self.skt.sendto(self.gestor_paquete.pasarPaqueteABytes(paquete_ack),(self.ip_cliente,self.puerto_cliente))
             #Receiver.cerrarArchivo(self.ruta_archivo)
             self.conexion_activa = False
-        
+        """
         if (self.gestor_paquete.verificar_mensaje_recibido(paquete) == True):
             paquete_ack = self.gestor_paquete.crearPaqueteACK(1)
             self.skt.sendto(self.gestor_paquete.pasarPaqueteABytes(paquete_ack),(self.ip_cliente,self.puerto_cliente))
